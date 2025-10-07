9f486816480936936c142694229ffecf56365bf1
Mariana Coronado <maricoronad19@gmail.com>
2025-10-07 08:57:08 -0300

    update Bru

--- content.js
+++ content.js
@@ -2,28 +2,11 @@ const container = document.createElement("div");
 container.classList.add("div-guia");
 document.body.appendChild(container);
 
-// const botaoConfig = document.createElement("button");
-// botaoConfig.innerText = "Configurar";
-// botaoConfig.classList.add("botao-config");
-// balao.appendChild(botaoConfig);
+const botaoConfig = document.createElement("button");
+botaoConfig.innerText = "Configurar";
+botaoConfig.classList.add("botao-config");
+balao.appendChild(botaoConfig);
 
-
-
-const balaoBru = document.createElement("div");
-// balao.src = chrome.runtime.getURL("images/assistente/balao_fala.png");
-balaoBru.classList.add("balaoBru");
-container.appendChild(balaoBru);
-
-// const textoBalao = document.createElement("div");
-// textoBalao.innerText = "Olá! Eu sou a Bru, sua assistente de acessibilidade. Estou aqui para ajudar você a personalizar sua experiência de navegação na web. Vamos começar?";
-// textoBalao.classList.add("texto-balao");
-// balao.appendChild(textoBalao);
-
-
-// const guia = document.createElement("img");
-// guia.src = chrome.runtime.getURL("images/assistente/guia_bru.png");
-// guia.classList.add("img-guia");
-// container.appendChild(guia);
 
 // botaoConfig.addEventListener("click", () => {
 //   alert("Aqui você pode configurar as opções de acessibilidade.");
